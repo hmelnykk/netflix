271e1bbda34620bc8b83a8d7dd7b02664ba7a48c
hmelnykk <151665742+hmelnykk@users.noreply.github.com>
2024-10-09 14:17:28 +0300

    test husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   plugins: [
-    require('daisyui'),
   ],
   theme: {
     extend: {
